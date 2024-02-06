aef75bc21bd83ad758f8f3fd3414ecdc24e1526a
Ashfaqul Awal <ashfaq.awal@gmail.com>
2024-02-06 06:51:07 +0100

    Adding comments regarding artist class.

--- API/Models/Artist.cs
+++ API/Models/Artist.cs
@@ -7,11 +7,23 @@ namespace API.Models
 {
   public class Artist
   {
-    public int Id { get; set; }
-    public string Name { get; set; }
-    public string? Description { get; set; }
+        public int Id { get; set; } // artist's unique Id
+        public string Name { get; set; } // artist's Name
+        public string? Description { get; set; }
 
-    public virtual ICollection<User> Users { get; set; }
-    public virtual ICollection<Song> Songs { get; set; }
+        /* A nullable string property representing a description or additional 
+         * information about the artist. The ? after string in indicates that 
+         * the Description property can be nullable. */
+
+        public virtual ICollection<User> Users { get; set; }
+
+        /* The ICollection<User> and ICollection<Song> properties imply a 
+         * one-to-many relationship between an artist and users or songs. 
+         * An artist can be associated with multiple users and songs.
+         * 
+         * The virtual keyword indicates that this property can be overridden 
+         * by derived classes.*/
+
+        public virtual ICollection<Song> Songs { get; set; }
   }
 }
