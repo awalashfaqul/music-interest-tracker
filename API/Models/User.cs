66b2046f8c9b9ad37de84b9ae34b976937d89606
Ashfaqul Awal <ashfaq.awal@gmail.com>
2024-02-06 06:56:05 +0100

    adding comments

--- API/Models/User.cs
+++ API/Models/User.cs
@@ -13,5 +13,10 @@ namespace API.Models
     public virtual ICollection<Artist> Artists { get; set; }
     public virtual ICollection<Genre> Genres { get; set; }
     public virtual ICollection<Song> Songs { get; set; }
-  }
+        /* The ICollection<Artist>, ICollection<Genre> and ICollection<Song> properties imply a 
+         * relationship between an artist, genre or song. 
+         * 
+         * The virtual keyword indicates that this property can be overridden 
+         * by derived classes.*/
+    }
 }
